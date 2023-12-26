25c20305a26d534f68c6cead6b093f4e452b5e87
Manu-Andaluz <manupeandaluz@gmail.com>
2023-12-26 02:23:40 -0300

    fixing indentation errors

--- Queue.py
+++ Queue.py
@@ -1,36 +1,33 @@
 class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
-	def __init__(self, value):
-		self.value = value
-		self.next = None
 
 class Queue:
-
-	def __init__(self):
-		self.head = self.tail = None
+    def __init__(self):
+        self.head = None
+        self.tail = None
         self.length = 0
 
-	def EnQueue(self, item):
-		node = Node(item)
+    def EnQueue(self, item):
+        node = Node(item)
         self.length += 1
 
-		if self.tail == None:
-			self.head = self.tail = node
-			return
-		self.tail.next = node
-		self.tail = node
+        if self.tail is None:
+            self.head = self.tail = node
+            return
+        self.tail.next = node
+        self.tail = node
 
-	def DeQueue(self):
-        
-		if self.isEmpty():
-			return
+    # Method to remove an item from the queue
+    def DeQueue(self):
+        if self.isEmpty():
+            return
 
         self.length -= 1
-		head = self.head
-		self.head = self.head.next
+        head = self.head
+        self.head = self.head.next
 
-		if(self.head == None):
-			self.tail = None
-
-
-
+        if self.head is None:
+            self.tail = None
--- Stack.py
+++ Stack.py
@@ -1,31 +1,32 @@
 from typing import Optional
 
+
 class Node:
-    def __init__(self,value):
-        self.value = value;
-        self.prev : Optional["Node"] = None
+    def __init__(self, value):
+        self.value = value
+        self.prev: Optional["Node"] = None
+
 
 class Stack:
-
     def __init__(self):
         self.length = 0
         self.head = None
 
     def push(self, value):
         new_node = Node(value)
-        if self.head == None:
+        if self.head is None:
             self.length += 1
             self.head = new_node
             return
-        else :
+        else:
             new_node.prev = self.head
             self.head = new_node
 
     def pop(self):
-        if self.head == None:
+        if self.head is None:
             head = self.head
             self.head = None
-            return  head.value if head else None
+            return head.value if head else None
 
         self.length -= 1
         head = self.head
@@ -34,5 +35,3 @@ class Stack:
 
     def peek(self):
         return self.head.value if self.head else None
-        
-
