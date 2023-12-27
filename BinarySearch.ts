6c31dea5ac3dc0b2a593881ee47eaaea5b340c5d
Manu-Andaluz <manupeandaluz@gmail.com>
2023-12-27 02:11:44 -0300

    adding binary search in typescript

---
+++ BinarySearch.ts
@@ -0,0 +1,23 @@
+export const BinarySearch = (array: number[], valueToFind): boolean => {
+  let leftPointer = 0;
+  let rightPointer = array.length - 1;
+
+  do {
+    const pointer = Math.floor((leftPointer + rightPointer) / 2);
+    const currentValue = array[pointer];
+
+    if (currentValue == valueToFind) {
+      return true;
+    } else if (currentValue > valueToFind) {
+      rightPointer = pointer - 1;
+    } else {
+      leftPointer = pointer + 1;
+    }
+  } while (leftPointer <= rightPointer);
+  return false;
+};
+
+BinarySearch([1, 2, 3, 4, 5], 5);
+BinarySearch([1, 2, 3, 4, 5], 2);
+BinarySearch([1, 2, 3, 4, 5], 3);
+BinarySearch([1, 2, 3, 4, 5], 1);
