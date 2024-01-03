f0316f71a040c6165cf50a017b596eca8b7b0bf8
Manu-Andaluz <manupeandaluz@gmail.com>
2024-01-03 00:29:58 -0300

    fixing errors

--- __tests__/DoublyLinkedList.ts
+++ __tests__/DoublyLinkedList.ts
@@ -1,7 +1,17 @@
 import DoublyLinkedList from "../DoublyLinkedList.ts";
 import LikedList from "../DoublyLinkedList.ts";
 
-export function test_list(list: DoublyLinkedList<number>): void {
+type List<T> = {
+  get length(): number;
+  removeAt(index: number): T | undefined;
+  remove(item: T): T | undefined;
+  get(index: number): T | undefined;
+  prepend(item: T): void;
+  append(item: T): void;
+  insertAt(item: T, idx: number): void;
+};
+
+export function test_list(list: List<number>): void {
   list.append(5);
   list.append(7);
   list.append(9);
