5d3416199e9193ffa8684737cabdac161cba67e7
Manu-Andaluz <manupeandaluz@gmail.com>
2024-01-05 19:34:40 -0300

    adding python testing

--- Pycode/QuickSort.py
+++ Pycode/QuickSort.py
@@ -31,10 +31,24 @@ def partition(arr: List[int], low: int, high: int) -> int:
     return index
 
 
-def quick_sort(arr: List[int]) -> None:
+def quick_sort(arr: List[int]) -> List[int]:
     qs(arr, 0, len(arr) - 1)
+    print('last', arr)
+    return arr
 
 
-quick_sort([9, 3, 7, 4, 69, 420, 42])
-quick_sort([9, 99, 4, 1, 3, 8])
+#quick_sort([9, 3, 7, 4, 69, 420, 42])
+#quick_sort([9, 99, 4, 1, 3, 8])
 # quick_sort([9, 3, 7, 4, 69, 420, 42])
+#
+import unittest
+
+class TestListMethod(unittest.TestCase):
+    def test_doubly_linked_list(self):
+        list = [9, 3, 7, 4, 69, 420, 42]
+        order_list = quick_sort(list)
+        self.assertEqual(order_list,[3,4,7,9,42,69,420])
+
+
+if __name__ == '__main__':
+    unittest.main()
