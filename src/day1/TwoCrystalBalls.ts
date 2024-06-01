83044f79a20ea59535b5060a386dc9c288562099
Manu-Andaluz <manupeandaluz@gmail.com>
2024-06-01 18:08:39 -0300

    two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,26 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    // jump the square root of n && breaks[index] === true
+    // then walkf from the prev starting point,
+    // and find where is the ofther breaks[index] === true
+    let prevStart = 0;
+    let root = Math.round(Math.sqrt(breaks.length));
+    let pivot = root;
 
+    do {
+        const current = breaks[pivot];
+
+        if (current) {
+            for (let i = prevStart; i < pivot; i++) {
+                if (breaks[i]) {
+                    return i;
+                }
+            }
+        } else {
+            prevStart = pivot + 1;
+            pivot += root;
+        }
+    } while (pivot <= breaks.length - 1);
+
+    return -1;
 }
+
