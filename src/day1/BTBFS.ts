bee8fc9aeafcc655c2861d12882072408c75622c
Manu-Andaluz <manupeandaluz@gmail.com>
2024-06-07 17:58:15 -0300

    Binary tree breadth first seatch using queue

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,19 +1,26 @@
+import Queue from "@code/Queue";
+
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
-    const q: (BinaryNode<number> | null)[] = [head];
+    const queue = new Queue();
+    queue.enqueue(head);
 
-    while (q.length) {
-        const curr = q.shift() as BinaryNode<number> | null;
-        if (!curr) {
-            continue;
+    while (queue.peek() !== undefined) {
+        let curr = queue.deque() as BinaryNode<number>;
+
+        if (curr) {
+            if (curr.value === needle) {
+                return true;
+            }
+
+            if (curr.left) {
+                queue.enqueue(curr.left);
+            }
+
+            if (curr.right) {
+                queue.enqueue(curr.right);
+            }
         }
-
-        if (curr.value === needle) {
-            return true;
-        }
-
-        q.push(curr.left);
-        q.push(curr.right);
     }
+
     return false;
 }
-
