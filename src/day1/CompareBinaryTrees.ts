9acd34e88368d6b4559ee5c743ac20f924f32732
Manu-Andaluz <manupeandaluz@gmail.com>
2024-06-09 16:20:21 -0300

    compare binary trees

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,7 +1,8 @@
-export default function compare(
-    a: BinaryNode<number> | null,
-    b: BinaryNode<number> | null,
-): boolean {
+export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+    if (a?.value !== b?.value) {
+        return false;
+    }
+
     if (a === null && b === null) {
         return true;
     }
@@ -10,10 +11,5 @@ export default function compare(
         return false;
     }
 
-    if (a.value !== b.value) {
-        return false;
-    }
-
     return compare(a.left, b.left) && compare(a.right, b.right);
 }
-
