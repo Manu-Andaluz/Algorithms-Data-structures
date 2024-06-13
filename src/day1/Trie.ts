33ea23693346a22208575b4e042aa90ee3aad788
Manu-Andaluz <manupeandaluz@gmail.com>
2024-06-13 00:34:39 -0300

    Trie fixed

--- src/day1/Trie.ts
+++ src/day1/Trie.ts
@@ -1,47 +1,100 @@
-type Node = {
-    value: string;
-    key: string;
-    childrend?: { [key: string]: Node };
+class Node {
+    value: string | null;
+    children: Array<Node | null>;
     isEndOfWord: boolean;
-};
+
+    constructor(value: string | null = null) {
+        this.value = value;
+        this.children = new Array(26).fill(null);
+        this.isEndOfWord = false;
+    }
+}
 
 export default class Trie {
-    private data: Node[];
-    private head?: Node;
+    private head: Node;
     public height: number;
 
     constructor() {
-        this.data = [];
-        this.head = undefined;
+        this.head = new Node();
         this.height = 0;
     }
 
-    insert(word: string): void {}
+    insert(word: string): void {
+        let current = this.head;
 
-    delete(item: string): void {}
+        for (let i = 0; i < word.length; i++) {
+            const idx = this.getIdx(word[i]);
+
+            if (current.children[idx] === null) {
+                current.children[idx] = new Node(word[i]);
+            }
+
+            current = current.children[idx]!;
+        }
+
+        current.isEndOfWord = true;
+    }
+
+    walk(node: Node | null, deph: number, word: string): boolean {
+        if (!node) {
+            return false;
+        }
+
+        if (deph === word.length) {
+            if (node.isEndOfWord) {
+                node.isEndOfWord = false;
+            }
+
+            return node.children.every((child) => child === null);
+        }
+
+        const idx = this.getIdx(word[deph]);
+
+        if (this.walk(node.children[idx], deph + 1, word)) {
+            node.children[idx] = null;
+
+            return !node.isEndOfWord && node.children.every((child) => child === null);
+        }
+
+        return false;
+    }
+
+    delete(item: string): void {
+        this.walk(this.head, 0, item);
+    }
 
     getIdx(str: string): number {
         return str.charCodeAt(0) - "a".charCodeAt(0);
     }
 
-    find(partial: string): string[] | undefined {
-        return;
-        // if (this.data.length === 0) {
-        //     return [];
-        // }
-        //
-        // const path: string[] = [];
-        //
-        // for (let i = 0; i < partial.length; i++) {
-        //     const character = this.getIdx(partial[i]);
-        //
-        //     if (!this.data[character]) {
-        //         return undefined;
-        //     }
-        //
-        //     path.push(this.data[character]);
-        // }
-        //
-        // return path;
+    dfs(node: Node, prefix: string, arr: string[]): void {
+        if (node.isEndOfWord) {
+            arr.push(prefix);
+        }
+
+        for (let i = 0; i < node.children.length; i++) {
+            if (node.children[i]) {
+                this.dfs(node.children[i]!, prefix + node.children[i]!.value, arr);
+            }
+        }
+    }
+
+    find(partial: string): string[] {
+        let current = this.head;
+        const words: string[] = [];
+        let path = "";
+
+        for (let i = 0; i < partial.length; i++) {
+            const idx = this.getIdx(partial[i]);
+            if (!current.children[idx]) {
+                return [];
+            }
+            current = current.children[idx]!;
+            path += partial[i];
+        }
+
+        this.dfs(current, path, words);
+
+        return words;
     }
 }
